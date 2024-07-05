6da577ecbc06996e0c8616019a4751b64d5ace75
jerrydevelop <hs@192.168.1.70>
2024-07-05 17:56:48 -0600

    build: v1.0.0-rc

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,17 @@
 // interfaces
-import { ProductCardHOCProps } from "../interfaces/interfaces";
+import { ProductCardHOCProps } from '../interfaces/interfaces';
 
 // ----------------------------------------------------------------------
 
-import { ProductCard as ProductCardHOC } from "./ProductCard";
+import { ProductCard as ProductCardHOC } from './ProductCard';
 
-import { ProductButtons } from "./ProductButtons";
-import { ProductImage } from "./ProductImage";
-import { ProductTitle } from "./ProductTitle";
+import { ProductButtons } from './ProductButtons';
+import { ProductImage } from './ProductImage';
+import { ProductTitle } from './ProductTitle';
 
-export { ProductButtons } from "./ProductButtons";
-export { ProductImage } from "./ProductImage";
-export { ProductTitle } from "./ProductTitle";
+export { ProductButtons } from './ProductButtons';
+export { ProductImage } from './ProductImage';
+export { ProductTitle } from './ProductTitle';
 
 // ----------------------------------------------------------------------
 
